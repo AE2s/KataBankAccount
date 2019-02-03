89b989f048eeb181d88d925ab8eac667fe7a20f3
Anas <anas.essaghir@gmail.com>
2019-02-03 15:57:10 +0100

    Tests of balance account and deposit positive numbers

---
+++ BankAccount/Account.cs
@@ -0,0 +1,22 @@
+﻿namespace BankAccount
+{
+    public class Account
+    {
+        private double money;
+
+        public Account()
+        {
+            money = 0;
+        }
+
+        public double GetBalance()
+        {
+            return money;
+        }
+
+        public void Deposit(double money)
+        {
+            this.money += money;
+        }
+    }
+}
---
+++ BankAcountTest/AccountTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Principal;
+using BankAccount;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankAcountTest
+{
+    [TestClass]
+    public class AccountTest
+    {
+        [TestMethod]
+        public void Given_balance_of_new_account_should_return_zero()
+        {
+            Account account=new Account();
+            Assert.AreEqual(0,account.GetBalance());
+        }
+
+        [TestMethod]
+        public void Given_fifty_should_return_fifty_in_balance_of_account()
+        {
+            Account account=new Account();
+            account.Deposit(50);
+            Assert.AreEqual(50, account.GetBalance());
+        }
+
+        [TestMethod]
+        public void Given_two_deposit_should_return_sum_of_both_in_the_balance()
+        {
+            Account account=new Account();
+            account.Deposit(50);
+            account.Deposit(50);
+            Assert.AreEqual(100, account.GetBalance());
+        }
+    }
+}
